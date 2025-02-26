facce718d14a6c2ad00f4d23a5ead728075dcb26
Nipun <nipun.sankalana@gmail.com>
2025-02-26 20:29:14 +0530

    relase 1 changes

--- routes/api.php
+++ routes/api.php
@@ -12,26 +12,45 @@ use App\Http\Controllers\LotteryDashboardController;
 use App\Http\Controllers\LotteryListController;
 use App\Http\Controllers\PublicController;
 use App\Models\Lotteries;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-
-
-// Route::get('/landing-page-data', function () {
-//     return response()->json([
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => \Illuminate\Foundation\Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
 
 
-// Route::get('/lottery/{id}', [LotteriesController::class, 'index']);
-// Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
-// Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+        ->name('password.request');
+
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
+
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+        ->name('password.reset');
+
+    Route::post('reset-password', [NewPasswordController::class, 'store'])
+        ->name('password.store');
+});
+
+
+
+
+
 
 
 Route::middleware(['web'])->group(function () {
