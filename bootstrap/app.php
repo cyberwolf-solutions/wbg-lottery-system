4816ccaabd9673dbe5d539c15d0696ade48db701
dmunasingha <dunix00@gmail.com>
2025-03-01 17:55:28 +0530

    Add admin authentication middleware and update routes for admin access

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@
 //             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
 //         ]);
 
-      
+
 //         $middleware->api(prepend: [
 //             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
 //             'throttle:api',
@@ -28,17 +28,16 @@
 //         //
 //     })->create();
 
-
-
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -47,7 +46,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
+        // Register Admin Middleware
+        $middleware->alias([
+            'admin' => AdminMiddleware::class, // Register alias for middleware
+        ]);
         // Admin API
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
