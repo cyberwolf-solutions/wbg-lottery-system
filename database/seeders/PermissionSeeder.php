481742cb5284c5f7b89f87a324dd78767b39ede1
Nipun <nipun.sankalana@gmail.com>
2025-04-04 12:30:20 +0530

    ok

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -9,33 +9,34 @@ use Spatie\Permission\Models\Role;
 class PermissionSeeder extends Seeder
 {
     public function run(): void
-{
-    $permissions = [
-        'view users',
-        'view dashboard',
-        'manage users',
-        'manage affiliates',
-        'manage roles',
-        'manage players',
-        'manage lotteries',
-        'manage dashboards',
-        'view results',
-        'manage winners',
-        'manage credits',
-        'manage withdrawals',
-        'view purchases',
-        'view wallet history'
-    ];
+    {
+        $permissions = [
+            'view users',
+            'view dashboard',
+            'manage users',
+            'manage notices',
+            'manage affiliates',
+            'manage roles',
+            'manage players',
+            'manage lotteries',
+            'manage dashboards',
+            'view results',
+            'manage winners',
+            'manage credits',
+            'manage withdrawals',
+            'view purchases',
+            'view wallet history'
+        ];
 
-    foreach ($permissions as $permissionName) {
-        Permission::firstOrCreate(['name' => $permissionName, 'guard_name' => 'admin']);
+        foreach ($permissions as $permissionName) {
+            Permission::firstOrCreate(['name' => $permissionName, 'guard_name' => 'admin']);
+        }
+
+        // Assign permissions to roles
+        $adminRole = Role::firstOrCreate(['name' => 'Admin', 'guard_name' => 'admin']);
+        $adminRole->syncPermissions($permissions);
+
+        $editorRole = Role::firstOrCreate(['name' => 'Editor', 'guard_name' => 'admin']);
+        $editorRole->syncPermissions(['view dashboard', 'view users', 'view results']);
     }
-
-    // Assign permissions to roles
-    $adminRole = Role::firstOrCreate(['name' => 'Admin', 'guard_name' => 'admin']);
-    $adminRole->syncPermissions($permissions);
-    
-    $editorRole = Role::firstOrCreate(['name' => 'Editor', 'guard_name' => 'admin']);
-    $editorRole->syncPermissions(['view dashboard', 'view users', 'view results']);
-}
 }
